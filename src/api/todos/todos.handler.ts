1de70af21bd9c88c6dedf52ccea5fa7fb99f64ea
bhaveek424 <jainbhaveek.v727@gmail.com>
2023-01-02 20:44:04 +0530

    error handling

--- src/api/todos/todos.handler.ts
+++ src/api/todos/todos.handler.ts
@@ -1,8 +1,16 @@
-import { Response, Request } from 'express';
+import { Response, Request, NextFunction } from 'express';
 import { Todos, TodoWithId } from './todos.model';
 
-export async function findAll(req: Request, res: Response<TodoWithId[]>) {
-  const result = await Todos.find();
-  const todos = await result.toArray();
-  res.json(todos);
+export async function findAll(
+  req: Request,
+  res: Response<TodoWithId[]>,
+  next: NextFunction,
+) {
+  try {
+    const result = await Todos.find();
+    const todos = await result.toArray();
+    res.json(todos);
+  } catch (error) {
+    next(error);
+  }
 }
