f7edc0297eea5418ae95f3604a055640663456fb
bhaveek424 <jainbhaveek.v727@gmail.com>
2023-01-03 12:34:31 +0530

    request validation middleware refactor

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,6 +1,32 @@
 import { NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
 
 import ErrorResponse from './interfaces/ErrorResponse';
+import RequestValidator from './interfaces/RequestValidators';
+
+export function validateRequest(validators: RequestValidator) {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      if (validators.body) {
+        req.body = await validators.body.parseAsync(req.body);
+      }
+
+      if (validators.params) {
+        req.params = await validators.params.parseAsync(req.params);
+      }
+
+      if (validators.query) {
+        req.query = await validators.query.parseAsync(req.query);
+      }
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        res.status(422);
+      }
+      next(error);
+    }
+  };
+}
 
 export function notFound(req: Request, res: Response, next: NextFunction) {
   res.status(404);
@@ -9,7 +35,11 @@ export function notFound(req: Request, res: Response, next: NextFunction) {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function errorHandler(err: Error, req: Request, res: Response<ErrorResponse>, next: NextFunction) {
+export function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response<ErrorResponse>,
+) {
   const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
   res.status(statusCode);
   res.json({
