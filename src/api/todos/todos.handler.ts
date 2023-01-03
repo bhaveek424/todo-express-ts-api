4f0079b5800f0290e683bd24c14da7af6476c609
bhaveek424 <jainbhaveek.v727@gmail.com>
2023-01-03 11:33:03 +0530

    createOne handler

--- src/api/todos/todos.handler.ts
+++ src/api/todos/todos.handler.ts
@@ -1,5 +1,6 @@
 import { Response, Request, NextFunction } from 'express';
-import { Todos, TodoWithId } from './todos.model';
+import { InsertOneResult } from 'mongodb';
+import { Todo, Todos, TodoWithId } from './todos.model';
 
 export async function findAll(
   req: Request,
@@ -10,6 +11,20 @@ export async function findAll(
     const result = await Todos.find();
     const todos = await result.toArray();
     res.json(todos);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function createOne(
+  req: Request<{}, InsertOneResult<Todo>, Todo>,
+  res: Response<InsertOneResult<Todo>>,
+  next: NextFunction,
+) {
+  try {
+    const validateResult = await Todo.parse(req.body);
+    const insertResult = await Todos.insertOne(validateResult);
+    res.json(insertResult);
   } catch (error) {
     next(error);
   }
